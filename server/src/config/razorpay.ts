87302858228d2827f0a02e881a96751183ff1096
tejasrastogileet <tejasrastogi456@gmail.com>
2026-01-24 02:50:47 +0530

    Fix Razorpay env variable names and add PORT to render config

--- server/src/config/razorpay.ts
+++ server/src/config/razorpay.ts
@@ -4,8 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const instance = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY || '',
-    key_secret: process.env.RAZORPAY_SECRET || ''
+    key_id: process.env.RAZORPAY_KEY_ID || '',
+    key_secret: process.env.RAZORPAY_KEY_SECRET || ''
 });
 
 export default instance;
